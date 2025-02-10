bd87b85fa92d8824dc17c006631d3b7c74016608
Augusto Honorata <contatoaugustohonorata@gmail.com>
2025-02-10 18:41:47 -0300

    inicialize project

---
+++ src/Desafio1/Ninjas.java
@@ -0,0 +1,23 @@
+package Desafio1;
+
+public class Ninjas {
+    public static void main(String[] args) {
+        String Ninja1 = "Kushimo Takanari";
+        String Ninja2 = "Makino Mutarari";
+        String Ninja3 = "Pushido Rimaki";
+        int IdadeNinja1 = 17;
+        int IdadeNinja2 = 16;
+        int IdadeNinja3 = 14;
+        String Missao= "Vigiar a aldeia";
+        int NivelDeDificuladade = 7;
+        boolean MissaoConcluida = false;
+        System.out.println(Ninja1);
+        System.out.println(Ninja2);
+        System.out.println(Ninja3);
+
+    }
+}
+
+
+
+
